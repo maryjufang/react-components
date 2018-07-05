fb8d4503be48d420f7806c6a41e6325e59337ea3
Mary Chen <machen@mbp-machen.local>
2018-07-05 15:15:14 -0700

    use absolute import path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import NavBar from './components/layout/nav-bar.js';
-import NavItem from './components/layout/nav-item.js';
-import Body from './components/layout/body.js';
-import SideBar from './components/layout/side-bar.js';
-import Section from './components/layout/section.js';
-import Footer from './components/layout/footer.js';
-import './App.css';
+import NavBar from 'components/layout/nav-bar.js';
+import NavItem from 'components/layout/nav-item.js';
+import Body from 'components/layout/body.js';
+import SideBar from 'components/layout/side-bar.js';
+import Section from 'components/layout/section.js';
+import Footer from 'components/layout/footer.js';
+import 'App.css';
 
 class App extends Component {
   render() {
